0922bf77d6720af0165182c8f556adf462440e0f
Mikhail Potter <114878489+MikhailPotter@users.noreply.github.com>
2025-03-08 10:20:31 +0300

    Time: 40 ms (19.35%), Space: 16.3 MB (100%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,6 +1,12 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        i = 1
-        while i ** 2 <= x:
-            i += 1
-        return i - 1
+        left = 0
+        right = x
+        while right>=left:
+            mid = (left + right)//2
+
+            if mid*mid<=x:
+                left = mid+1
+            else:
+                right = mid-1       
+        return right
