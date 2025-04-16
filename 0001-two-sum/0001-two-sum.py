ac4171dba6497699052a85163437322488f48893
Mikhail Potter <114878489+MikhailPotter@users.noreply.github.com>
2025-04-16 13:41:55 +0300

    Time: 0 ms (100%), Space: 18.7 MB (62.87%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hmap = {}
-        for i, val in enumerate(nums):
-            if val in hmap:
-                return hmap[val], i
-            else:
-                hmap[target - val] = i
+        set_nums = set(nums)
+        for i, value in enumerate(nums):
+            if target - value in set_nums and i != nums.index(target-value):
+                res = [i, nums.index(target-value)]
+                return res
+        
+                
